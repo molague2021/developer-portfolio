82d7cc0d0dd5b6fc713de527b2fdfc8ef2e03000
Martin Olague <molague23@hotmail.com>
2023-08-08 21:03:08 -0700

    Fixed styles for textfield mui

--- src/containers/PortfolioFooter/ContactMeSection.tsx
+++ src/containers/PortfolioFooter/ContactMeSection.tsx
@@ -2,105 +2,141 @@ import React from 'react';
 import { Stack, Grid, Typography, styled, TextField } from '@mui/material';
 
 const StyledTitle = styled(Typography)`
-  color: white;
-  font-feature-settings: 'clig' off, 'liga' off;
+	color: white;
+	font-feature-settings: 'clig' off, 'liga' off;
 
-  /* Heading (XL) */
-  &.MuiTypography-root {
-    font-family: Space Grotesk;
-    font-size: 88px;
-    font-style: normal;
-    font-weight: 700;
-    line-height: 88px; /* 100% */
-    letter-spacing: -2.5px;
-  }
+	/* Heading (XL) */
+	&.MuiTypography-root {
+		font-family: Space Grotesk;
+		font-size: 88px;
+		font-style: normal;
+		font-weight: 700;
+		line-height: 88px; /* 100% */
+		letter-spacing: -2.5px;
+	}
 `;
 
 const StyledSubtitle = styled(Typography)`
-  width: 445px;
-  color: white;
-  text-align: left;
-  font-feature-settings: 'clig' off, 'liga' off;
+	width: 445px;
+	color: white;
+	text-align: left;
+	font-feature-settings: 'clig' off, 'liga' off;
 
-  /* Body */
-  font-family: Space Grotesk;
-  font-size: 18px;
-  font-style: normal;
-  font-weight: 500;
-  line-height: 28px; /* 155.556% */
+	/* Body */
+	font-family: Space Grotesk;
+	font-size: 18px;
+	font-style: normal;
+	font-weight: 500;
+	line-height: 28px; /* 155.556% */
 `;
 
-const StyledTextfield = styled(TextField)({
-  //     `
-  //     color: white !important;
-  //     font-family: Space Grotesk;
-  //     font-size: 16px;
-  //     font-style: normal;
-  //     font-weight: 500;
-  //     line-height: 26px; /* 162.5% */
-  //     letter-spacing: -0.222px;
-  //     opacity: 0.5;
-  //   `;
+const StyledTextField = styled(TextField)`
+	width: 445px;
+	.MuiInput-input {
+		color: white;
+		padding: 0 24px 17px;
+		font-family: Space Grotesk;
+		font-size: 16px;
+		font-style: normal;
+		font-weight: 500;
+		line-height: 26px; /* 162.5% */
+		letter-spacing: -0.222px;
+		opacity: 0.5;
+	}
+	.MuiInput-root:before {
+		border-bottom: 1px solid white;
+	}
+	.MuiInput-root:after {
+		border-bottom: 1px solid #4ee1a0;
+	}
+	.MuiInput-root:hover {
+		border-bottom: 1px solid #4ee1a0;
+	}
+	.MuiInput-root:hover:after {
+		border-bottom: none;
+		transition: none;
+	}
+`;
 
-  root: {
-    '& .MuiFormLabel-root': {
-      color: 'red', // or black
-    },
-  },
-});
-
+const StyledMultiLineTextField = styled(TextField)`
+	width: 445px;
+	.MuiInput-input {
+		color: white;
+		padding: 0 24px 17px;
+		font-family: Space Grotesk;
+		font-size: 16px;
+		font-style: normal;
+		font-weight: 500;
+		line-height: 26px; /* 162.5% */
+		letter-spacing: -0.222px;
+		opacity: 0.5;
+	}
+	.MuiInput-root:before {
+		border-bottom: 1px solid white;
+	}
+	.MuiInput-root:after {
+		border-bottom: 1px solid #4ee1a0;
+	}
+	.MuiInput-root:hover {
+		border-bottom: 1px solid #4ee1a0;
+	}
+	.MuiInput-root:hover:after {
+		border-bottom: none;
+		transition: none;
+	}
+`;
 const SUBTITLE = `I would love to hear about your project and how I could help. Please fill in the form, and I’ll get back to you as soon as possible.`;
 
 export const ContactMeSection = () => {
-  return (
-    <Grid
-      item
-      display="flex"
-      justifyContent="space-between"
-      sx={{ width: '1110px', height: '327px', flexShrink: 0 }}
-    >
-      <Stack
-        display="flex"
-        justifyContent="center"
-        alignItems="flex-start"
-        gap="36px"
-        sx={{ width: '445px', height: '208px' }}
-      >
-        <StyledTitle>Contact</StyledTitle>
-        <StyledSubtitle>{SUBTITLE}</StyledSubtitle>
-      </Stack>
-      <Stack
-        display="flex"
-        justifyContent="center"
-        alignItems="flex-start"
-        gap="36px"
-        sx={{ width: '445px', height: '208px' }}
-      >
-        <StyledTextfield
-          color="primary"
-          id="name-textfield"
-          placeholder="NAME"
-          variant="standard"
-          size="small"
-          sx={{
-            '&.MuiTextField-root': { color: 'white' },
-            '&.MuiInputBase-root-MuiInput-root': { color: 'white' },
-            width: '445px',
-          }}
-          InputLabelProps={{ sx: { ...{ color: 'white' } } }}
-          InputProps={{ sx: { ...{ borderBottom: '1px solid white' } } }}
-        />
-        <StyledTextfield
-          id="email-textfield"
-          label="Email"
-          variant="standard"
-        />
-        <StyledTextfield
-          id="message-textfield"
-          label="Message"
-          variant="standard"
-        />
-      </Stack>
-    </Grid>
-  );
+	return (
+		<Grid
+			item
+			display='flex'
+			justifyContent='space-between'
+			sx={{ width: '1110px', height: '327px', flexShrink: 0 }}
+		>
+			<Stack
+				display='flex'
+				justifyContent='center'
+				alignItems='flex-start'
+				gap='36px'
+				sx={{ width: '445px', height: '208px' }}
+			>
+				<StyledTitle>Contact</StyledTitle>
+				<StyledSubtitle>{SUBTITLE}</StyledSubtitle>
+			</Stack>
+			<Stack
+				display='flex'
+				justifyContent='center'
+				alignItems='flex-start'
+				gap='36px'
+				sx={{ width: '445px', height: '208px' }}
+			>
+				<Stack>
+					<StyledTextField
+						color='primary'
+						id='name-textfield'
+						placeholder='NAME'
+						variant='standard'
+					/>
+				</Stack>
+				<Stack>
+					<StyledTextField
+						id='email-textfield'
+						placeholder='EMAIL'
+						variant='standard'
+					/>
+				</Stack>
+				<Stack>
+					<StyledTextField
+						id='message-textfield'
+						multiline
+						rows={4}
+						placeholder='MESSAGE'
+						variant='standard'
+					/>
+				</Stack>
+			</Stack>
+		</Grid>
+	);
 };
